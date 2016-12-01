9e938648c9d41eb06efc459793df155de4fd0ad9
Amy Roberson <amy@roberson.xyz>
2016-11-30 21:02:25 -0500

    Add two versons of FizzBuzz

--- Math&Logic.playground/Pages/FizzBuzz2.xcplaygroundpage/Contents.swift
+++ Math&Logic.playground/Pages/FizzBuzz2.xcplaygroundpage/Contents.swift
@@ -1,7 +1,26 @@
-//: [Previous](@previous)
 
-import Foundation
+enum Answer {
+    case fizz
+    case buzz
+    case number(Int)
+    case fizzBuzz
+}
 
-var str = "Hello, playground"
+func transform(n: Int) -> Answer {
+    
+    if n % 5 == 0 && n % 3 == 0 {
+        return .fizzBuzz
+    } else if n % 5 == 0 {
+        return .buzz
+    } else if n % 3 == 0 {
+        return .fizz
+    } else {
+        return .number(n)
+    }
+}
 
-//: [Next](@next)
+transform(n: 7)
+transform(n: 15)
+transform(n: 21)
+transform(n: 3)
+transform(n: 5)
---
+++ Math&Logic.playground/Pages/FizzBuzz3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+enum Answer {
+    case fizz
+    case buzz
+    case number(Int)
+    case fizzBuzz
+}
+
+func transform(n: Int) -> Answer {
+    switch (n % 5 == 0, n % 3 == 0) {
+        case (false, true):
+            return .fizz
+        case (true, false):
+            return .buzz
+        case (true, true):
+            return .fizzBuzz
+        default:
+            return .number(n)
+    }
+}
+transform(n: 7)
+transform(n: 15)
+transform(n: 21)
+transform(n: 3)
+transform(n: 5)
